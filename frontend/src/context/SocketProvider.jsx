c21369bbd90edb6aafe4133f4427b9e11d50dcb9
Aditya Giri <adityagiri1911@gmail.com>
2024-06-29 01:20:59 +0530

    Server deplyoed link added to frontend

--- frontend/src/context/SocketProvider.jsx
+++ frontend/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8000"), []);
+  const socket = useMemo(() => io("https://hack4bengal-1.onrender.com"), []);
 
   return (
     <SocketContext.Provider value={socket}>
